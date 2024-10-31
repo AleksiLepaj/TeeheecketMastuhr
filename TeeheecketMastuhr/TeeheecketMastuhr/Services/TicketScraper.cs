f9a37c084e2cfaadcf2814b1c476663ddbd18979
Alexis <2175497@cegepmontpetit.ca>
2024-10-30 22:38:34 -0400

    FCT TicketScraper Service

---
+++ TeeheecketMastuhr/TeeheecketMastuhr/Services/TicketScraper.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using TeeheecketMastuhr.Models;
+
+namespace TeeheecketMastuhr.Services
+{
+    public class TicketScraper
+    {
+        private readonly HttpClient _httpClient;
+
+        public TicketScraper(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Ticket>> ScrapeTicketsAsync(string url)
+        {
+            var response = await _httpClient.GetStringAsync(url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response);
+
+            var tickets = new List<Ticket>();
+
+            var nodes = doc.DocumentNode.SelectNodes("//li[@data-bdd='quick-picks-list-item-resale']");
+            foreach (var node in nodes)
+            {
+                var section = node.SelectSingleNode(".//span[@title='Section Description']").InnerText.Split('•')[0].Replace("Sec ", "").Trim();
+                var row = node.SelectSingleNode(".//span[@title='Section Description']").InnerText.Split('•')[1].Replace("Row ", "").Trim();
+                var priceString = node.SelectSingleNode(".//button[@data-bdd='quick-pick-price-button']").InnerText.Replace("CA $", "").Trim();
+                var price = double.Parse(priceString);
+
+                tickets.Add(new Ticket
+                {
+                    Section = section,
+                    Row = row,
+                    Price = price
+                });
+            }
+            return tickets;
+        }
+    }
+}
