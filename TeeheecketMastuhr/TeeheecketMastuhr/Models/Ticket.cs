ff7295a3aa53448bae665d4e4f8f0b7527428975
Alexis <2175497@cegepmontpetit.ca>
2024-10-30 22:28:07 -0400

    INIT Ajout Projet
    Ajout projet, création classe Ticket

---
+++ TeeheecketMastuhr/TeeheecketMastuhr/Models/Ticket.cs
@@ -0,0 +1,9 @@
+﻿namespace TeeheecketMastuhr.Models
+{
+    public class Ticket
+    {
+        public string Section { get; set; }
+        public string Row { get; set; }
+        public double Price { get; set; }
+    }
+}
